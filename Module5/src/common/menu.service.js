4e54ff23e48bd663e58e7be9979aa0192b97b2f6
Kamal Memon <kamalmemon11@gmail.com>
2016-10-30 12:42:02 -0300

    Menu Service remove http config headers

--- Module5/src/common/menu.service.js
+++ Module5/src/common/menu.service.js
@@ -1,51 +1,48 @@
 (function () {
-"use strict";
+  "use strict";
 
-angular.module('common')
-.service('MenuService', MenuService);
-
-
-MenuService.$inject = ['$http', 'ApiPath'];
-function MenuService($http, ApiPath) {
-  var service = this;
-
-  service.getCategories = function () {
-    return $http.get(ApiPath + '/categories.json').then(function (response) {
-      return response.data;
-    });
-  };
+  angular.module('common')
+    .service('MenuService', MenuService);
 
 
-  service.getMenuItems = function (category) {
-    var config = {};
-    if (category) {
-      config.params = {'category': category};
-    }
+  MenuService.$inject = ['$http', 'ApiPath'];
+  function MenuService($http, ApiPath) {
+    var service = this;
 
-    return $http.get(ApiPath + '/menu_items.json', config).then(function (response) {
-      return response.data;
-    });
-  };
-
-  
-  service.getMenuItem = function (shortName) {  
-    var config = {headers:  {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET, POST',
-        "Access-Control-Allow-Headers" : "X-Requested-With"
-    }
-};
-    return $http.get(ApiPath + '/menu_items/' + shortName + '.json', config).then(function (response) {
-      return response.data;
-    });
-   /* .catch( function (response) {
-        console.log("number not found");
+    service.getCategories = function () {
+      return $http.get(ApiPath + '/categories.json').then(function (response) {
         return response.data;
-    });*/
-  };
+      });
+    };
 
-}
+    service.getMenuItems = function (category) {
+      var config = {};
+      if (category) {
+        config.params = { 'category': category };
+      }
+
+      return $http.get(ApiPath + '/menu_items.json', config).then(function (response) {
+        return response.data;
+      });
+    };
+
+    service.getMenuItem = function (shortName) {
+      return $http.get(ApiPath + '/menu_items/' + shortName + '.json').then(function (response) {
+        return response.data;
+      });
+    };
 
 
-
+    /*  service.getMenuItem = function (shortName) {  
+        var config = {headers:  {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET, POST',
+            "Access-Control-Allow-Headers" : "X-Requested-With"
+        }
+    };
+        return $http.get(ApiPath + '/menu_items/' + shortName + '.json', config).then(function (response) {
+          return response.data;
+        });
+      };*/
+  }
 })();
